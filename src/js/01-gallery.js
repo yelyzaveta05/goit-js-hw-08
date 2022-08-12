9fb43d8aabc0ae8fb919c319f54087acd7b792f5
Yelyzaveta Demchenko <97131904+yelyzaveta05@users.noreply.github.com>
2022-08-12 23:52:32 +0300

    done 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = createGallery(galleryItems);
+
+function createGallery(array) {
+    return array.map(({ preview, original, description }) => {
+        return `<a class="gallery__item"  href="${original}">
+    <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+    }).join('');
+};
+
+const simpleLightBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
